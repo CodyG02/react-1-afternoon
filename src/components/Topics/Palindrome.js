f6574c785d39782769501a7de40dbc03e8f787b6
Cody Garn <garn.cody@gmail.com>
2020-03-26 19:50:37 -0500

    completed

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,56 @@
 import React, {Component} from 'react'
 
 class Palindrome extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            userInput: '',
+            palindrome: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({userInput: event.target.value})
+    }
+    
+
+
+    handleClick(){
+        // This method should update the value of palindrome.
+        // This method should solve the toy problem.
+    
+        let userInputArr = this.state.userInput.split('');
+        console.log(userInputArr);
+    
+        let userInputArrReversed = userInputArr.reverse();
+        console.log(userInputArrReversed)
+        
+        let userInputReversedJoined = userInputArrReversed.join('');
+        console.log(userInputReversedJoined)
+    
+        
+        if( userInputReversedJoined === this.state.userInput ){
+          this.setState({
+            palindrome: 'true'
+          })
+        } else {
+          this.setState({
+            palindrome: 'false'
+          })
+        }
+      }
+
+
     render(){
         return(
-            <p>Palindrome Component</p>
+            <div className='puzzleBox filterStringPB'>
+                <h4 >Palindrome</h4>
+                <input className='inputLine' onChange={(event)=> this.handleChange(event)}/>
+                <button className='confirmationButton' onClick={() => this.handleClick()}>Cllick Me</button>
+                <span className='resultBox'>Palindrome: {this.state.palindrome}</span>
+            </div>
         )
     }
 }
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,11 +1,101 @@
 import React, {Component} from 'react'
 
 class Sum extends Component {
-    render(){
-        return(
-            <p>Sum Component</p>
-        )
+constructor(){
+    super()
+
+    this.state={
+        number1: 0,
+        number2: 0,
+        sum: null
     }
 }
 
+handleInputOne(event){
+    this.setState({
+        number1: +event.target.value
+    })
+}
+
+handleInputTwo(event){
+    this.setState({
+        number2: +event.target.value
+    })
+}
+
+handleClick(){
+    this.setState({
+        sum: this.state.number1 + this.state.number2
+    })
+}
+
+render(){
+    return(
+        <div className='puzzleBox sumPb'>
+            <h4>Sum</h4>
+            <input className='inputLine' onChange={(event) => this.handleInputOne(event)}/>
+            <input className='inputLine' onChange={(event) => this.handleInputTwo(event)}/>
+           
+            <button className='confirmationButton' onClick={() => this.handleClick()} >Click</button>
+            <span className='resultsBox'>Sum:{this.state.sum}</span>
+        </div>
+    )
+}
+
+}
+
 export default Sum
+
+// import React, { Component } from 'react';
+
+// export default class Sum extends Component {
+//   constructor(){
+//     super()
+
+//     this.state = {
+//       number1: 0,
+//       number2: 0,
+//       sum: null
+//     }
+//   }
+
+//   handleInputOne(event){
+//     this.setState({
+//       number1: +event.target.value
+//     })
+//   }
+
+//   handleInputTwo(event){
+//     this.setState({
+//       number2: +event.target.value
+//     })
+//   }
+
+//   // handleClick(){
+
+//   // }
+
+//   handleClick = () => {
+//     // This method should update the value of sum.
+//     // This method should solve the toy problem.
+
+//     this.setState({
+//       sum: this.state.number1 + this.state.number2
+//     })
+//   }
+
+//   render() {
+//     console.log(typeof this.state.number1)
+//     console.log(typeof this.state.number2)
+//     console.log(typeof this.state.sum)
+//     return (
+//       <div className="puzzleBox sumPB">
+//         <h4>Sum</h4>
+//         <input className="inputLine" onChange={(event) => this.handleInputOne(event)} />
+//         <input className="inputLine" onChange={(event) => this.handleInputTwo(event)} />
+//         <button className="confirmationButton" onClick={this.handleClick} >Click Me!</button>
+//     <span className="resultsBox">Sum: {this.state.sum}</span>
+//       </div>
+//     )
+//   }
+// }
